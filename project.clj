f5f3ec018d5fc39650573c2245a93c1c735da5b0
Devin Walters <devinw@gmail.com>
2013-03-08 16:52:06 -0600

    Add iota for parallel file processing, bump to clojure 1.5

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://getclojure.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [clojurewerkz/elastisch "1.0.2"]
+                 [iota "1.0.2"]
                  [clojail "1.0.3"]
                  [org.clojars.semperos/enlive "1.0.1"
                   :exclusions [org.clojure/clojure]]
-                 [me.raynes/laser "0.1.15"]
                  [org.thnetos/cd-client "0.3.6"]
                  ;; Web
                  [lib-noir "0.3.5"]
