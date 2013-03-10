2524aa322cdcc4b2c63a8d9d28a24af3608af40f
Devin Walters <devinw@gmail.com>
2013-03-10 17:14:14 -0500

    Update to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://getclojure.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojurewerkz/elastisch "1.0.2"]
                  [iota "1.0.2"]
                  [clojail "1.0.3"]
