1200bb610348bd1edd058346ae4fa5a06a1f50ea
Devin Walters <devinw@gmail.com>
2013-03-10 17:39:48 -0500

    Heroku, listen to what I tell you: Use the MONGOLAB_URI

--- src/getclojure/db.clj
+++ src/getclojure/db.clj
@@ -2,11 +2,16 @@
   (:require [monger.core :as mg]
             [monger.collection :as mc]))
 
+(defn get-db-uri []
+  (get (System/getenv)
+       "MONGOLAB_URI"
+       "mongodb://127.0.0.1/getclojure_development"))
+
 (defn make-connection! []
-  (let [mongolab (get (System/getenv) "MONGOLAB_URI" false)
-        db-uri (or mongolab "mongodb://127.0.0.1/getclojure_development")]
+  (let [db-uri (get-db-uri)
+        environment (if (.contains db-uri "heroku") "production" "development")]
     (mg/connect-via-uri! db-uri)
-    (if mongolab
+    (if (.contains db-uri "heroku")
       (mg/use-db! "getclojure")
       (mg/use-db! "getclojure_development"))
     (mc/ensure-index "sexps" {:user 1})
@@ -14,4 +19,4 @@
     (mc/ensure-index "sexps" {:raw-input 1} {:unique true})
     (mc/ensure-index "sexps" {:raw-output 1})
     (mc/ensure-index "sexps" {:raw-value 1})
-    {:environment (if mongolab "production" "development")}))
+    {:environment environment}))
