da75489d92d5c4cf11594372a3ffbddb4a6e62c6
Devin Walters <devinw@gmail.com>
2013-05-16 20:30:38 -0500

    Fix typo in analytics tag

--- src/getclojure/views/layout.clj
+++ src/getclojure/views/layout.clj
@@ -63,8 +63,7 @@
   [:head
    [:title "GetClojure"]
    (include-css "/css/screen.css" "/css/github.css")
-   [:script "<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
