616329b00c622dcb2f1cf7454127fcf36e56058d
Devin Walters <devinw@gmail.com>
2013-05-16 20:22:08 -0500

    Add Google analytics

--- src/getclojure/views/layout.clj
+++ src/getclojure/views/layout.clj
@@ -62,7 +62,15 @@
 (defhtml base [& content]
   [:head
    [:title "GetClojure"]
-   (include-css "/css/screen.css" "/css/github.css")]
+   (include-css "/css/screen.css" "/css/github.css")
+   [:script "<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41005509-1', 'getclojure.org');
+  ga('send', 'pageview');"]]
   [:body content
    (include-js "//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js")])
 
