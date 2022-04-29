69b3931e88eb0dc45bbce1cf0a650905a0565dbf
Devin Walters <devinw@gmail.com>
2022-04-29 14:30:03 -0500

    Add routes ns

---
+++ src/getclojure/routes.clj
@@ -0,0 +1,23 @@
+(ns getclojure.routes
+  (:require
+   [getclojure.views.layout :as layout]
+   [compojure.core :refer [defroutes GET]]
+   [compojure.route :as route]))
+
+(defn homepage
+  []
+  (layout/common
+   (layout/search-form)))
+
+(defn search-page
+  [q num]
+  (layout/common
+   (layout/search-form q)
+   (layout/search-results q num)))
+
+(defroutes routes
+  (GET "/" [] (homepage))
+  (GET "/search" [q num]
+       (search-page q num))
+  (route/resources "/")
+  (route/not-found "Route not found"))
