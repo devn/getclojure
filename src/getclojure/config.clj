5f379466a25efffad782c21765e6aceb44f351e4
Devin Walters <devinw@gmail.com>
2022-04-29 14:14:19 -0500

    Removing old code

--- src/getclojure/config.clj
+++
@@ -1,6 +0,0 @@
-(ns getclojure.config
-  (:require [clj-config.core :as cfg]))
-
-(def config
-  "External configuration"
-  (cfg/safely cfg/read-config "config.clj"))
--- src/getclojure/queue.clj
+++
@@ -1,9 +0,0 @@
-(ns getclojure.queue)
-
-(defmethod print-method clojure.lang.PersistentQueue
-  [q, w]
-  (print-method '<- w)
-  (print-method (seq q) w)
-  (print-method '-< w))
-
-(defn make-queue [] clojure.lang.PersistentQueue/EMPTY)
