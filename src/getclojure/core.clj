44823f0c1cbd3369faf6cd6890a83c406bcb85e8
Devin Walters <devinw@gmail.com>
2022-04-30 13:56:40 -0500

    Set up an uncaught exception handler

--- src/getclojure/core.clj
+++ src/getclojure/core.clj
@@ -4,6 +4,11 @@
    [ring.adapter.jetty :as ring]
    [ring.middleware.defaults :as ring.defaults]))
 
+(Thread/setDefaultUncaughtExceptionHandler
+ (reify Thread$UncaughtExceptionHandler
+   (uncaughtException [_ thread ex]
+     (println ex "Uncaught exception on" (.getName thread)))))
+
 (def app (ring.defaults/wrap-defaults
           #'routes/routes
           ring.defaults/site-defaults))
