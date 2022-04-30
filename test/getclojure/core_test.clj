fe0b874429673c7ab0bbd3b7ef31f3e11287cea8
Devin Walters <devinw@gmail.com>
2022-04-30 14:11:24 -0500

    Rename core_test.clj -> server_test.clj

--- test/getclojure/core_test.clj
+++
@@ -1,3 +0,0 @@
-(ns getclojure.core-test
-  (:require [clojure.test :as test :refer (deftest testing is)]
-            [getclojure.core :as sut]))
---
+++ test/getclojure/server_test.clj
@@ -0,0 +1,3 @@
+(ns getclojure.server-test
+  (:require [clojure.test :as test :refer (deftest testing is)]
+            [getclojure.server :as sut]))
